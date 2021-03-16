4d860c1f7ba746747d99df27edc76747f86a2366
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-03-16 16:25:18 -0700

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,4 @@
 jQuery(function ($) {
-  var seconds = $("#timerNum");
-  var timer;
-  function myFunction() {
-  
-      if (seconds > 0) { // so it doesn't go to -1
-          seconds--;
-      }
-      if (seconds == 0) {
-       $(".spin").css("display","none") 
-      }
-  }
-  if (!timer) {
-      timer = window.setInterval(function () {
-          myFunction();
-      }, 1000); // every second
-  }
+
 });
 
